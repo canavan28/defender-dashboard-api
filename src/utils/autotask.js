a4283d7e67e5925da7d42336dadc7f340e672cdb
canavan28 <matthew@infotank.com>
2026-05-07 10:46:34 -0400

    Fix empty headers debug

--- src/utils/autotask.js
+++ src/utils/autotask.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 
+const BASE_URL = () => `${process.env.AUTOTASK_ZONE_URL}/v1.0`;
+
 function getHeaders() {
   return {
     'Content-Type': 'application/json',
@@ -9,38 +11,19 @@ function getHeaders() {
   };
 }
 
-const BASE_URL = () => `${process.env.AUTOTASK_ZONE_URL}/v1.0`;
-
 const autotaskClient = {
-  post: (path, data) => {
-    return axios({
-      method: 'post',
-      url: `${BASE_URL()}${path}`,
-      data,
-      transformRequest: [(data, headers) => {
-        headers['UserName'] = process.env.AUTOTASK_USERNAME;
-        headers['Secret'] = process.env.AUTOTASK_SECRET;
-        headers['ApiIntegrationCode'] = process.env.AUTOTASK_INTEGRATION_CODE;
-        headers['Content-Type'] = 'application/json';
-        return JSON.stringify(data);
-      }]
-    });
-  },
-  get: (path) => axios({
-    method: 'get',
-    url: `${BASE_URL()}${path}`,
-    headers: getHeaders()
-  })
+  post: (path, data) => axios.post(`${BASE_URL()}${path}`, data, { headers: getHeaders() }),
+  get: (path) => axios.get(`${BASE_URL()}${path}`, { headers: getHeaders() })
 };
 
-// Temporary debug interceptor - remove once working
+// Temporary debug interceptor
 axios.interceptors.request.use(request => {
   if (request.url && request.url.includes('autotask')) {
     console.log('[AutoTask Request URL]', request.url);
     console.log('[AutoTask Headers]', JSON.stringify({
-      UserName: request.headers['UserName'],
-      SecretLength: request.headers['Secret']?.length,
-      ApiIntegrationCode: request.headers['ApiIntegrationCode']
+      UserName: request.headers['UserName'] || request.headers['username'],
+      SecretLength: (request.headers['Secret'] || request.headers['secret'])?.length,
+      ApiIntegrationCode: request.headers['ApiIntegrationCode'] || request.headers['apiintegrationcode']
     }));
   }
   return request;
