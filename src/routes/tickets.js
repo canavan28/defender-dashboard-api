2fdcf1540093232b0d926179e1651aed891d71ee
canavan28 <matthew@infotank.com>
2026-05-08 12:14:10 -0400

    Add queue and status filters to ticket queries

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -4,26 +4,64 @@ const { autotaskClient } = require('../utils/autotask');
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+// Queues to include
+const INCLUDE_QUEUES = [5, 29682833, 29683482, 29683496, 29683497];
+
+// Statuses to exclude
+const EXCLUDE_STATUSES = [5, 20]; // Complete, RMM Resolved
+
 router.get('/all', async (req, res, next) => {
   try {
+    // All tickets in valid queues
     const summaryRes = await autotaskClient.post('/Tickets/query', {
-      filter: [{ field: 'id', op: 'gt', value: 0 }]
+      filter: [
+        {
+          op: 'or',
+          items: INCLUDE_QUEUES.map(id => ({
+            field: 'queueID',
+            op: 'eq',
+            value: id
+          }))
+        }
+      ]
     });
-
-    // Temporary - log first ticket to see field names
-    if (summaryRes.data.items?.length > 0) {
-      console.log('[Ticket Fields]', JSON.stringify(summaryRes.data.items[0]));
-    }
-
     await sleep(500);
 
+    // Open tickets in valid queues (excluding RMM Resolved and Complete)
     const openRes = await autotaskClient.post('/Tickets/query', {
-      filter: [{ field: 'status', op: 'noteq', value: 5 }]
+      filter: [
+        {
+          op: 'or',
+          items: INCLUDE_QUEUES.map(id => ({
+            field: 'queueID',
+            op: 'eq',
+            value: id
+          }))
+        },
+        {
+          op: 'and',
+          items: EXCLUDE_STATUSES.map(id => ({
+            field: 'status',
+            op: 'noteq',
+            value: id
+          }))
+        }
+      ]
     });
     await sleep(500);
 
+    // Categories in valid queues
     const categoriesRes = await autotaskClient.post('/Tickets/query', {
-      filter: [{ field: 'id', op: 'gt', value: 0 }]
+      filter: [
+        {
+          op: 'or',
+          items: INCLUDE_QUEUES.map(id => ({
+            field: 'queueID',
+            op: 'eq',
+            value: id
+          }))
+        }
+      ]
     });
 
     res.json({
@@ -35,14 +73,5 @@ router.get('/all', async (req, res, next) => {
     next(err);
   }
 });
-router.get('/queues', async (req, res, next) => {
-  try {
-    const response = await autotaskClient.get('/Tickets/entityInformation/fields');
-    const fields = response.data.fields || [];
-    const queueField = fields.find(f => f.name === 'queueID');
-    res.json({ queues: queueField?.picklistValues || [] });
-  } catch (err) {
-    next(err);
-  }
-});
+
 module.exports = router;
